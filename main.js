1af751c7dc3e642cf49b10c7c843ceb46f82b04d
yroush <32183395+yroush@users.noreply.github.com>
2019-03-20 21:35:56 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,97 +1,100 @@
+var sprite1X = 1;
+var sprite1Y = 1;
+
+var sprite2X = 8;
+var sprite2Y = 8;
+
 function checkBounds (sprite) {
   return true;
 }
 
-function move (sprite, direction) {
+function move1 (sprite, direction) {
   // set css styles grid-column and grid-row
   if (checkBounds(sprite) === true) {
     if (direction === 'up') {
-
+      sprite1Y--;
+      // console.log(sprite.style.gridRowStart);
+      sprite.style.gridRowStart = sprite1Y;
+      sprite.style.gridRowEnd = sprite1Y;
+      sprite.style.gridColumnStart = sprite1X;
+      sprite.style.gridColumnEnd = sprite1X;
     } else if (direction === 'down') {
-
+      sprite1Y++;
+      // console.log(sprite.style.gridRowStart);
+      sprite.style.gridRowStart = sprite1Y;
+      sprite.style.gridRowEnd = sprite1Y;
+      sprite.style.gridColumnStart = sprite1X;
+      sprite.style.gridColumnEnd = sprite1X;
     } else if (direction === 'left') {
-
+      sprite1X--;
+      sprite.style.gridRowStart = sprite1Y;
+      sprite.style.gridRowEnd = sprite1Y;
+      sprite.style.gridColumnStart = sprite1X;
+      sprite.style.gridColumnEnd = sprite1X;
     } else if (direction === 'right') {
-
+      sprite1X++;
+      sprite.style.gridRowStart = sprite1Y;
+      sprite.style.gridRowEnd = sprite1Y;
+      sprite.style.gridColumnStart = sprite1X;
+      sprite.style.gridColumnEnd = sprite1X;
     }
   }
 }
 
-document.getElementById('main').addEventListener('keyup', event => {
-  let sprite1 = document.getElementById('sprite1');
-  let sprite2 = document.getElementById('sprite2');
+function move2 (sprite, direction) {
+  // set css styles grid-column and grid-row
+  if (checkBounds(sprite) === true) {
+    if (direction === 'up') {
+      sprite2Y--;
+      // console.log(sprite.style.gridRowStart);
+      sprite.style.gridRowStart = sprite2Y;
+      sprite.style.gridRowEnd = sprite2Y;
+      sprite.style.gridColumnStart = sprite2X;
+      sprite.style.gridColumnEnd = sprite2X;
+    } else if (direction === 'down') {
+      sprite2Y++;
+      // console.log(sprite.style.gridRowStart);
+      sprite.style.gridRowStart = sprite2Y;
+      sprite.style.gridRowEnd = sprite2Y;
+      sprite.style.gridColumnStart = sprite2X;
+      sprite.style.gridColumnEnd = sprite2X;
+    } else if (direction === 'left') {
+      sprite2X--;
+      sprite.style.gridRowStart = sprite2Y;
+      sprite.style.gridRowEnd = sprite2Y;
+      sprite.style.gridColumnStart = sprite2X;
+      sprite.style.gridColumnEnd = sprite2X;
+    } else if (direction === 'right') {
+      sprite2X++;
+      sprite.style.gridRowStart = sprite2Y;
+      sprite.style.gridRowEnd = sprite2Y;
+      sprite.style.gridColumnStart = sprite2X;
+      sprite.style.gridColumnEnd = sprite2X;
+    }
+  }
+}
 
-  if (event.keyCode === 87) {
-    // w
-    move(sprite1, 'up');
-  } else if (event.keyCode === 65) {
-    // a
-    move(sprite1, 'left');
-  } else if (event.keyCode === 83) {
-    // s
-    move(sprite1, 'down');
-  } else if (event.keyCode === 68) {
-    // d
-    move(sprite1, 'right');
-  } else if (event.keyCode === 73) {
-    // i
-    move(sprite2, 'up');
-  } else if (event.keyCode === 74) {
-    // j
-    move(sprite2, 'left');
-  } else if (event.keyCode === 75) {
-    // k
-    move(sprite2, 'down');
-  } else if (event.keyCode === 76) {
-    // l
-    move(sprite2, 'right');
+document.querySelector('html').addEventListener('keyup', (event) => {
+  console.log(event.key);
+  let sprite1 = document.querySelector('#sprite1');
+  console.log(sprite1);
+  let sprite2 = document.querySelector('#sprite2');
+
+  if (event.key === 'w') {
+    move1(sprite1, 'up');
+  } else if (event.key === 'a') {
+    move1(sprite1, 'left');
+  } else if (event.key === 's') {
+    move1(sprite1, 'down');
+  } else if (event.key === 'd') {
+    move1(sprite1, 'right');
+  } else if (event.key === 'i') {
+    move2(sprite2, 'up');
+  } else if (event.key === 'j') {
+    move2(sprite2, 'left');
+  } else if (event.key === 'k') {
+    move2(sprite2, 'down');
+  } else if (event.key === 'l') {
+    move2(sprite2, 'right');
   }
 });
-
-
-
-
-////////////////////////////////////////
-
-function player1moveup() {
-  move(sprite1, 'up');
-}
-
-function player1movedown() {
-  move(sprite1, 'down');
-}
-
-function player1moveleft() {
-  move(sprite1, 'left');
-}
-
-function player1moveright() {
-  move(sprite1, 'right');
-}
-
-function player1moveup() {
-  move(sprite2, 'up');
-}
-
-function player1movedown() {
-  move(sprite2, 'down');
-}
-
-function player1moveleft() {
-  move(sprite2, 'left');
-}
-
-function player1moveright() {
-  move(sprite2, 'right');
-}
-
-<button onclick="player1moveup()">W</button>
-<button onclick="player1movedown()">S</button>
-<button onclick="player1moveleft()">A</button>
-<button onclick="player1moveright()">D</button>
-
-<button onclick="player2moveup()">I</button>
-<button onclick="player2movedown()">K</button>
-<button onclick="player2moveleft()">J</button>
-<button onclick="player2moveright()">L</button>
-//////////////////////////
